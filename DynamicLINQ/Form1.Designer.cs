7ce71820158db542d6691a257cd6e89ef895488f
joelee2017 <as955133@gmail.com>
2017-12-18 15:41:31 +0800

    push

--- DynamicLINQ/Form1.Designer.cs
+++ DynamicLINQ/Form1.Designer.cs
@@ -28,12 +28,89 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.btnSqlQuery = new System.Windows.Forms.Button();
+            this.btnExecuteSQLCommand = new System.Windows.Forms.Button();
+            this.dataGridView2 = new System.Windows.Forms.DataGridView();
+            this.btnCustOrderHist = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 41);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(769, 280);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // btnSqlQuery
+            // 
+            this.btnSqlQuery.Location = new System.Drawing.Point(37, 12);
+            this.btnSqlQuery.Name = "btnSqlQuery";
+            this.btnSqlQuery.Size = new System.Drawing.Size(75, 23);
+            this.btnSqlQuery.TabIndex = 1;
+            this.btnSqlQuery.Text = "查詢";
+            this.btnSqlQuery.UseVisualStyleBackColor = true;
+            this.btnSqlQuery.Click += new System.EventHandler(this.btnSqlQuery_Click);
+            // 
+            // btnExecuteSQLCommand
+            // 
+            this.btnExecuteSQLCommand.Location = new System.Drawing.Point(135, 11);
+            this.btnExecuteSQLCommand.Name = "btnExecuteSQLCommand";
+            this.btnExecuteSQLCommand.Size = new System.Drawing.Size(75, 23);
+            this.btnExecuteSQLCommand.TabIndex = 2;
+            this.btnExecuteSQLCommand.Text = "更新";
+            this.btnExecuteSQLCommand.UseVisualStyleBackColor = true;
+            this.btnExecuteSQLCommand.Click += new System.EventHandler(this.btnExecuteSQLCommand_Click);
+            // 
+            // dataGridView2
+            // 
+            this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView2.Location = new System.Drawing.Point(12, 328);
+            this.dataGridView2.Name = "dataGridView2";
+            this.dataGridView2.RowTemplate.Height = 24;
+            this.dataGridView2.Size = new System.Drawing.Size(769, 150);
+            this.dataGridView2.TabIndex = 3;
+            // 
+            // btnCustOrderHist
+            // 
+            this.btnCustOrderHist.Location = new System.Drawing.Point(238, 11);
+            this.btnCustOrderHist.Name = "btnCustOrderHist";
+            this.btnCustOrderHist.Size = new System.Drawing.Size(122, 23);
+            this.btnCustOrderHist.TabIndex = 4;
+            this.btnCustOrderHist.Text = "預存程序方法化";
+            this.btnCustOrderHist.UseVisualStyleBackColor = true;
+            this.btnCustOrderHist.Click += new System.EventHandler(this.btnCustOrderHist_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(793, 491);
+            this.Controls.Add(this.btnCustOrderHist);
+            this.Controls.Add(this.dataGridView2);
+            this.Controls.Add(this.btnExecuteSQLCommand);
+            this.Controls.Add(this.btnSqlQuery);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button btnSqlQuery;
+        private System.Windows.Forms.Button btnExecuteSQLCommand;
+        private System.Windows.Forms.DataGridView dataGridView2;
+        private System.Windows.Forms.Button btnCustOrderHist;
     }
 }
 
--- DynamicLINQ/Form1.cs
+++ DynamicLINQ/Form1.cs
@@ -4,6 +4,7 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Linq.Dynamic;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -15,6 +16,33 @@ namespace DynamicLINQ
         public Form1()
         {
             InitializeComponent();
+        }
+
+        //LINQ 中直接使用SQL語法，需安裝組件 System.Linq.Dynamic
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            NorthwindEntities dc = new NorthwindEntities();
+            dataGridView1.DataSource = dc.Customers.Where("Region !=null").OrderBy("Country").ToArray();
+        }
+
+        private void btnSqlQuery_Click(object sender, EventArgs e)
+        {
+            NorthwindEntities dc = new NorthwindEntities();
+            dataGridView1.DataSource = dc.Customers.SqlQuery("select * from Customers where Country={0}", "UK").ToArray();
+        }
+
+        private void btnExecuteSQLCommand_Click(object sender, EventArgs e)
+        {
+            NorthwindEntities dc = new NorthwindEntities();
+
+                dc.Database.ExecuteSqlCommand("Update Customers set City={0} where CustomerID={1}", "Taipeifuck", "ALFKI");
+            dataGridView2.DataSource = dc.Customers.OrderBy("Country").ToArray();
+        }
+
+        private void btnCustOrderHist_Click(object sender, EventArgs e)//預存程序 方法化從資料庫類型加入
+        {
+            NorthwindEntities dc = new NorthwindEntities();
+            dataGridView1.DataSource = dc.CustOrderHist("ALFKI").ToArray();
         }
     }
 }
